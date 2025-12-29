a7c387f0335b84ce996fa929af765502dddcdfc5
kaushik jagani <kaushik.200410116078@gmail.com>
2025-12-29 22:43:32 +0530

    add directives and variables access in html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Admin} from './Components/admin/admin';
+import {User} from './Components/user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Admin,User],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
